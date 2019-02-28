ed1855318d7938714f9d378f984d0622cdde09ed
Sxigames <sxigames63@gmail.com>
2019-02-27 21:48:28 -0300

    Convert to HTML

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import feedparser
 import wikipedia
 wikipedia.set_lang('pt')
-
+html = open('index.html', 'w', encoding='utf-8')
 Trends = feedparser.parse('https://trends.google.com/trends/trendingsearches/daily/rss?geo=BR')
 
+html.write("""<html>
+<head></head><body>""")
 for trend in Trends.entries:
     wiki_result = wikipedia.summary(trend.title)
+    html.write(f"""<p>{wiki_result}</p><br>""")
+
+
+html.write("""</body></html>""")
+html.close()
