835b574a32a39608f077b0da2f34bcda55b4f7cd
Sxigames <sxigames63@gmail.com>
2019-02-28 12:22:47 -0300

    Fix no results error

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ html = open('index.html', 'w', encoding='utf-8')
 Trends = feedparser.parse('https://trends.google.com/trends/trendingsearches/daily/rss?geo=BR')
 
 html.write("""<html>
-<head></head><body>""")
+<head></head><body><center><h1>Trends</h1></center><br><br><br>""")
 for trend in Trends.entries:
-    wiki_result = wikipedia.summary(trend.title)
-    html.write(f"""<p>{wiki_result}</p><br>""")
+    try:
+        wiki_result = wikipedia.summary(trend.title)
+        html.write(f"""<center><p>{trend.title}</p></center><br><p>{wiki_result}</p><br><br>""")
+    except:
+        html.write(f"""<center><p>{trend.title}</p></center><br><p>Não foi possivel  achar resultados sobre {trend.title} na 
+        WikiPedia</p><br><br>""")
 
 
 html.write("""</body></html>""")
