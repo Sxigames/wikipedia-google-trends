47adc3a643d3bc6257dbe0055230945b6c31dfce
Sxigames <sxigames63@gmail.com>
2019-04-10 15:49:41 -0300

    Ok, depois eu volto ao plano de defs

--- main.py
+++ main.py
@@ -1,34 +1,19 @@
 import feedparser
 import wikipedia
+wikipedia.set_lang('pt')
+html = open('index.html', 'w', encoding='utf-8')
+Trends = feedparser.parse('https://trends.google.com/trends/trendingsearches/daily/rss?geo=BR')
+
+html.write("""<html>
+<head></head><body><center><h1>Trends</h1></center><br><br><br>""")
+for trend in Trends.entries:
+    try:
+        wiki_result = wikipedia.summary(trend.title)
+        html.write(f"""<center><p>{trend.title}</p></center><br><p>{wiki_result}</p><br><br>""")
+    except:
+        html.write(f"""<center><p>{trend.title}</p></center><br><p>Não foi possivel  achar resultados sobre {trend.title} na 
+        WikiPedia</p><br><br>""")
 
 
-def get_wiki_results(feed_geo, wiki_lang):
-    wikipedia.set_lang(wiki_lang)
-    trends = feedparser.parse(f'https://trends.google.com/trends/trendingsearches/daily/rss?geo={feed_geo}')
-    wiki_results = []
-    for trend in trends.entries:
-        try:
-            wiki_result = wikipedia.summary(trend.title)
-            result = {trend.title, wiki_result}
-            wiki_results.extend([result])
-        except:
-            result = {trend.title, "No Result."}
-            wiki_results.extend([result])
-    return wiki_results
-
-
-def generate_html():
-    html = open('index.html', 'w', encoding='utf-8')
-    html.write("""<html>
-    <head></head><body><center><h1>Trends</h1></center><br><br><br>""")
-    for result in get_wiki_results('BR', 'pt') :
-        try:
-            html.write(f"""<center><p>{result.title}</p></center><br><p>{result.wiki_result}</p><br><br>""")
-        except:
-            html.write(f"""<center><p>{result.title}</p></center><br><p>Não foi possivel  achar resultados sobre {result.title} na
-            Wikipedia</p><br><br>""")
-    html.write("""</body></html>""")
-    html.close()
-
-
-generate_html()
+html.write("""</body></html>""")
+html.close()
